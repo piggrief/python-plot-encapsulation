4e6b7d60dce258d8a0793fb9f8fd7db06a888296
piggrief <zhuzhibeishang@126.com>
2020-02-25 12:53:40 +0800

    seaborn_plot:add violinplot function

--- seaborn_function/maintest.py
+++ seaborn_function/maintest.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
     rd = sp.read_data_to_plot(True)
     rd.read_data_from_xls(DataFilePath, FileName)
 
-    boxplot = sp.seaborn_box_plot(rd.origin_data, x_label='开始小时', y_label='收益',
-                                  title='在机场内一小时的平均收益随时间段变化图',
-                                  hue_label='机场单', y_range=(0, 65)
-                                  , background_style="whitegrid")
-    boxplot.plot()
+    seaborn_plot = sp.plot_with_seaborn(
+        rd.origin_data, x_label='开始小时', y_label='收益',
+        title='在机场内一小时的平均收益随时间段变化图',
+        hue_label='机场单', y_range=(0, 65)
+        , background_style="whitegrid", order=[1.0, 2.0])
+    seaborn_plot.violinplot(False).show()
 
     input()
--- seaborn_function/seaborn_plot.py
+++ seaborn_function/seaborn_plot.py
@@ -26,14 +26,32 @@ class read_data_to_plot:
         return True
 
 
-class seaborn_box_plot:
+class plot_with_seaborn:
     """
     seaborn的箱线图绘图常用封装
     """
     def __init__(self, data, x_label, y_label, title, hue_label=[], font_size=18,
-                 x_range=[], y_range=[], palette=[], background_style=[]):
+                 x_range=[], y_range=[], palette=[], background_style=[], order=[]):
         """
-        构造函数
+        boxplot构造函数，用来配置一些绘图参数
+        sample:
+        seaborn_plot = sp.plot_with_seaborn(rd.origin_data,
+                               x_label='开始小时', y_label='收益',
+                              title='在机场内一小时的平均收益随时间段变化图',
+                              hue_label='机场单', y_range=(0, 65)
+                              , background_style="whitegrid", order=[1.0, 2.0])
+
+        :param data:用于绘图的原始数据
+        :param x_label:x轴标签
+        :param y_label:y轴标签
+        :param title:图表标题
+        :param hue_label:划分标签
+        :param font_size:字体大小
+        :param x_range:x轴范围，如(0, 10.5)
+        :param y_range:y轴范围，如(0, 10.5)
+        :param palette:调色板，如“Set3”
+        :param background_style:背景样式，如"whitegrid"
+        :param order:显示的x轴的取值范围，也可理解为约束，如[1.0, 2.0]
         """
         self.origin_data = data
         self.x_label = x_label
@@ -44,26 +62,48 @@ class seaborn_box_plot:
         self.x_range = x_range
         self.y_range = y_range
         self.palette = palette
-
+        self.order = order
         if [] != background_style:
             sns.set(style=background_style, font='SimHei')
 
-    def plot(self):
+    def boxplot(self, if_show):
+        """
+        根据定义的参数绘制箱线图
+        :param if_show:是否直接显示，True的话会直接调用plt.show()，
+                                     False的话会返回plt句柄
+        :return:plt句柄，可用于后续对图表的自定义修改
+        """
         f, ax = plt.subplots()
         # ax = plt.subplot(2, 1, 1)
         if not self.hue_label:
             if [] == self.palette:
-                sns.boxplot(x=self.x_label, y=self.y_label, data=self.origin_data)
+                if [] == self.order:
+                    sns.boxplot(x=self.x_label, y=self.y_label, data=self.origin_data)
+                else:
+                    sns.boxplot(x=self.x_label, y=self.y_label, data=self.origin_data,
+                                order=self.order)
             else:
-                sns.boxplot(x=self.x_label, y=self.y_label, data=self.origin_data,
-                            palette=self.palette)
+                if [] == self.order:
+                    sns.boxplot(x=self.x_label, y=self.y_label, data=self.origin_data,
+                                palette=self.palette)
+                else:
+                    sns.boxplot(x=self.x_label, y=self.y_label, data=self.origin_data,
+                                palette=self.palette, order=self.order)
         else:
             if [] == self.palette:
-                sns.boxplot(x=self.x_label, y=self.y_label, hue=self.hue_label
-                            , data=self.origin_data)
+                if [] == self.order:
+                    sns.boxplot(x=self.x_label, y=self.y_label, hue=self.hue_label
+                                , data=self.origin_data)
+                else:
+                    sns.boxplot(x=self.x_label, y=self.y_label, hue=self.hue_label
+                                , data=self.origin_data, order=self.order)
             else:
-                sns.boxplot(x=self.x_label, y=self.y_label, hue=self.hue_label
-                            , data=self.origin_data, palette=self.palette)
+                if [] == self.order:
+                    sns.boxplot(x=self.x_label, y=self.y_label, hue=self.hue_label
+                                , data=self.origin_data, palette=self.palette)
+                else:
+                    sns.boxplot(x=self.x_label, y=self.y_label, hue=self.hue_label
+                                , data=self.origin_data, palette=self.palette, order=self.order)
         ax.set_title(self.title, fontsize=self.font_size)
 
         ax.set_xlabel(self.x_label, fontsize=self.font_size)
@@ -76,5 +116,66 @@ class seaborn_box_plot:
         plt.yticks(fontproperties='Times New Roman', size=self.font_size)
         plt.xticks(fontproperties='Times New Roman', size=self.font_size)
 
-        plt.show()
+        if if_show:
+            plt.show()
+            return []
+        else:
+            return plt
+
+    def violinplot(self, if_show, split=True):
+        """
+        根据定义的参数绘制箱线图
+        :param if_show:是否直接显示，True的话会直接调用plt.show()，
+                                     False的话会返回plt句柄
+        :param split:如果有分组的话是否对半拼接显示
+        :return:plt句柄，可用于后续对图表的自定义修改
+        """
+        f, ax = plt.subplots()
+        # ax = plt.subplot(2, 1, 1)
+        if not self.hue_label:
+            if [] == self.palette:
+                if [] == self.order:
+                    sns.violinplot(x=self.x_label, y=self.y_label, data=self.origin_data)
+                else:
+                    sns.violinplot(x=self.x_label, y=self.y_label, data=self.origin_data,
+                                   order=self.order)
+            else:
+                if [] == self.order:
+                    sns.violinplot(x=self.x_label, y=self.y_label, data=self.origin_data,
+                                   palette=self.palette)
+                else:
+                    sns.violinplot(x=self.x_label, y=self.y_label, data=self.origin_data,
+                                   palette=self.palette, order=self.order)
+        else:
+            if [] == self.palette:
+                if [] == self.order:
+                    sns.violinplot(x=self.x_label, y=self.y_label, hue=self.hue_label,
+                                   data=self.origin_data, split=split)
+                else:
+                    sns.violinplot(x=self.x_label, y=self.y_label, hue=self.hue_label,
+                                   data=self.origin_data, order=self.order, split=split)
+            else:
+                if [] == self.order:
+                    sns.violinplot(x=self.x_label, y=self.y_label, hue=self.hue_label,
+                                   data=self.origin_data, palette=self.palette, split=split)
+                else:
+                    sns.violinplot(x=self.x_label, y=self.y_label, hue=self.hue_label, split=split,
+                                   data=self.origin_data, palette=self.palette, order=self.order)
+        ax.set_title(self.title, fontsize=self.font_size)
+
+        ax.set_xlabel(self.x_label, fontsize=self.font_size)
+        ax.set_ylabel(self.y_label, fontsize=self.font_size)
+        if self.x_range:
+            plt.xlim(self.x_range)
+        if self.y_range:
+            plt.ylim(self.y_range)
+
+        plt.yticks(fontproperties='Times New Roman', size=self.font_size)
+        plt.xticks(fontproperties='Times New Roman', size=self.font_size)
+
+        if if_show:
+            plt.show()
+            return []
+        else:
+            return plt
 
